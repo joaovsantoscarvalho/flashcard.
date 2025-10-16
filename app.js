5270f98c4617eb4221ca758151fd95d4ea7feeb3
joaovsantoscarvalho <carvalho.joao08@escola.pr.gov.br>
2025-10-16 07:26:10 -0300

    .

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 function criaCartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container');
     let cartao = document.createElement('article');
-    cartao.className = 'cartao'
+    cartao.className = 'cartao';
 }
+
+cartao.innerHTML = 
+<div class="catao_conteudo">
+    <h3>Programação</h3>
+    <div class="cartao_conteudo_pergunta">
+        <p>O que é Javacript</p>
+    </div>
+</div>
