2037522df6932c20b4f7024fe6abf5f45cd3f4d6
joaovsantoscarvalho <carvalho.joao08@escola.pr.gov.br>
2025-10-16 07:30:44 -0300

    .

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ cartao.innerHTML =
 <div class="catao_conteudo">
     <h3>Programação</h3>
     <div class="cartao_conteudo_pergunta">
-        <p>O que é javacript</p>
+        <p>O que é javacript ?</p>
     </div>
     <div class="cartao_conteudo_resposta">
         <p>O javacript é uma linguagem de Programação</p>
